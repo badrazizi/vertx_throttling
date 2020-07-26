39fde566679bb14952161ec5ed8dd339b8a970ce
Badr <badrazizi3@gmail.com>
2020-07-26 12:39:09 +0300

    Update Throttl.kt

--- src/main/java/vertx/throttling/Throttl.kt
+++ src/main/java/vertx/throttling/Throttl.kt
@@ -4,8 +4,8 @@ import io.vertx.ext.web.Router
 
 interface Throttl {
   companion object {
-    fun getThrottling(vertx: Vertx, ips: List<String>? = null): ThrottlImpl {
-      return ThrottlImpl(vertx, ips)
+    fun getThrottling(vertx: Vertx, ips: List<String>? = null, customData: CustomData? = null): ThrottlImpl {
+      return ThrottlImpl(vertx, ips, customData)
     }
   }
 }
